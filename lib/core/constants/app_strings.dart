3b29c9ff2e48a29d1569df9677ae6016cf6075aa
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2026-02-27 10:32:23 +0600

    feat: implement sliver foundation for home page

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,12 @@
+class AppStrings {
+  static const String appName = 'Daraz Clone';
+  static const String searchHint = 'Search in Daraz';
+  static const String searchButton = 'Search';
+  static const String flashSale = 'Flash Sale';
+  static const String shopMore = 'Shop More';
+  static const String dailySheraDeals = 'Daily Shera Deals';
+  static const String topRanking = 'Top Ranking';
+  static const String forYou = 'For You';
+  static const String hotDeals = 'Hot Deals';
+  static const String voucherMax = 'Voucher Max';
+}
