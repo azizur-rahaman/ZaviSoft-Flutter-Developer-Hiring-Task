b0aa1e355b5e43d09dc661efe0fca4fdea3681d7
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2026-02-27 14:56:12 +0600

    feat(assets): integrate real promotional images and CachedNetworkImage

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  static const List<String> promoBanners = [
+    'https://img.lazcdn.com/us/lazada_mars_image/a5293d6090c9e530bc9c300205f18382.jpg_760x760q80.jpg_.avif',
+    'https://img.lazcdn.com/us/lazada_mars_image/1afd918691e36a27316a0d154dc88405.png_760x760q80.png_.avif',
+    'https://img.lazcdn.com/us/lazada_mars_image/e2d2be7e7025a6cb11524a5c8419fc48.jpg_760x760q80.jpg_.avif',
+    'https://img.lazcdn.com/us/lazada_mars_image/a304362ab3c80c6a8e6f0cf4832f5748.jpg_760x760q80.jpg_.avif',
+    'https://img.lazcdn.com/us/lazada_mars_image/ac33f20f4b8c0be2c4a2f77e8bea9ab6.png_760x760q80.png_.avif',
+    'https://img.lazcdn.com/us/lazada_mars_image/e5ef27e4124691f4c6293fe6872dc42f.jpg_760x760q80.jpg_.avif',
+  ];
+}
--- lib/features/home/presentation/widgets/promo_carousel.dart
+++ lib/features/home/presentation/widgets/promo_carousel.dart
@@ -1,46 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import '../../../../core/constants/app_assets.dart';
 
 class PromoCarousel extends StatelessWidget {
   const PromoCarousel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Mock data for banners
-    final List<String> banners = [
-      'https://placeholder.com/banner1.jpg',
-      'https://placeholder.com/banner2.jpg',
-      'https://placeholder.com/banner3.jpg',
-    ];
-
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10.h),
       child: CarouselSlider(
         options: CarouselOptions(
           height: 150.h,
-          viewportFraction: 0.9,
+          viewportFraction: 0.92,
           enlargeCenterPage: true,
           autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 4),
         ),
-        items: banners.map((url) {
-          return Builder(
-            builder: (BuildContext context) {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: Text(
-                    'Banner ${banners.indexOf(url) + 1}',
-                    style: TextStyle(fontSize: 16.sp),
+        items: AppAssets.promoBanners.map((url) {
+          return CachedNetworkImage(
+            imageUrl: url,
+            imageBuilder: (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.r),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.1),
+                    blurRadius: 8,
+                    offset: const Offset(0, 4),
                   ),
-                ),
-                // In a real app, use CachedNetworkImage here
-              );
-            },
+                ],
+              ),
+            ),
+            placeholder: (context, url) => Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(12.r),
+              ),
+              child: const Center(
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+            ),
+            errorWidget: (context, url, error) => Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(12.r),
+              ),
+              child: const Icon(Icons.error_outline, color: Colors.grey),
+            ),
           );
         }).toList(),
       ),
